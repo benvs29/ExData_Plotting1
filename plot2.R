f305c9327ce749ffddb11e869efbb30ab61b366d
benvs29 <ajmottes@gmail.com>
2026-03-31 10:45:22 -0400

    Solution for part 2

---
+++ plot2.R
@@ -0,0 +1,13 @@
+# Read February data (created from dataset using getFebData.R)
+library(data.table)
+df <- fread("./data/feb_household.csv")
+df <- df[, -1]
+
+png(filename = "plot2.png")
+par(mar = c(5.1, 5.1, 4.1, 2.1))
+dates_vec <- as.POSIXct(c("2007-02-01", "2007-02-02", "2007-02-03"), tz="UTC")
+labels_vec <- weekdays(dates_vec, abbreviate = TRUE)
+plot(df$Date, df$Global_active_power, type = "S", xaxt = "n",
+     xlab="", ylab="Global Active Power (kilowatts")
+axis(side = 1, at = dates_vec, labels = labels_vec)
+dev.off()
