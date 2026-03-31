04705688d2f7e3e3ded17488d682d04c49269fe0
benvs29 <ajmottes@gmail.com>
2026-03-31 11:10:56 -0400

    Fixed type in plot2.R

--- plot2.R
+++ plot2.R
@@ -9,6 +9,6 @@ par(mar = c(5.1, 5.1, 4.1, 2.1))
 dates_vec <- as.POSIXct(c("2007-02-01", "2007-02-02", "2007-02-03"), tz="UTC")
 labels_vec <- weekdays(dates_vec, abbreviate = TRUE)
 plot(df$Date, df$Global_active_power, type = "S", xaxt = "n",
-     xlab="", ylab="Global Active Power (kilowatts")
+     xlab="", ylab="Global Active Power (kilowatts)")
 axis(side = 1, at = dates_vec, labels = labels_vec)
 dev.off()
